5b91ec054cd85b92652fd29d28376836f5b817fa
Bijay Gurung <bijay.learning@gmail.com>
2022-07-31 14:47:33 +0200

    Modify plot_perf_params to support multiple tasks

--- AutoFormer/plot_perf_params.py
+++ AutoFormer/plot_perf_params.py
@@ -2,6 +2,8 @@ import argparse
 import pathlib
 import typing
 
+from collections import namedtuple
+
 import matplotlib.pyplot as plt
 from pylab import rcParams
 import pandas as pd
@@ -10,33 +12,35 @@ rcParams['font.size'] = 16
 rcParams['legend.fontsize'] = 20
 plt.style.use('ggplot')
 
-METRIC_PLOT_RANGE = {
-    'ppl': (12, 30)
+Metric = namedtuple("Metric", "col_name full_name plot_low plot_high")
+
+TASK_TO_METRIC = {
+    'cifar100': Metric("test_acc_1", "Accuracy@1", 60, 75),
+    'swinir': Metric("psnr", "PSNR", 23, 25),
+    'roberta': Metric("ppl", "MLM Perplexity", 12, 30),
 }
 
 
 def plot_all(evolution_input_path: typing.Union[str, pathlib.Path], 
              random_search_input_path: typing.Union[str, pathlib.Path],
              output_path: typing.Union[str, pathlib.Path],
+             metric: Metric,
              tag: str = None):
 
     output_path = pathlib.Path(output_path)
-    tag = f'_{tag}' if tag is not None else ''
+    tag = f'{tag}_' if tag is not None else ''
 
     ev_data_df = pd.read_json(evolution_input_path, lines=True)
     rs_data_df = pd.read_json(random_search_input_path, lines=True)
 
-    perf_col = 'ppl'
-    perf_metric = 'MLM Perplexity'
     OPACITY = 0.3
 
     def generate_scatter_plot(ev_data_df, rs_data_df, x_col, x_label, perf_col):
         fig, ax = plt.subplots(figsize=(14, 8))
 
         ax.set_xlabel(x_label, fontsize=16)
-        ax.set_ylabel(perf_metric, fontsize=16)
-        if perf_col in METRIC_PLOT_RANGE:
-            ax.set_ylim(*METRIC_PLOT_RANGE[perf_col])
+        ax.set_ylabel(metric.full_name, fontsize=16)
+        ax.set_ylim(metric.plot_low, metric.plot_high)
 
         ax.scatter(ev_data_df[x_col], ev_data_df[perf_col], alpha=OPACITY, c='tab:red', label='Evolution')
         ax.scatter(rs_data_df[x_col], rs_data_df[perf_col], alpha=OPACITY, c='tab:blue', label='Random')
@@ -53,35 +57,59 @@ def plot_all(evolution_input_path: typing.Union[str, pathlib.Path],
     ## Epoch vs Performance
     fig, ax = generate_scatter_plot(ev_data_df, rs_data_df, 
                                     x_col='epoch', x_label='Search Iteration', 
-                                    perf_col=perf_col)
+                                    perf_col=metric.col_name)
     # plt.tight_layout()
-    fig.savefig(output_path / f'{tag}perf_for_top_50_every_epoch.png', dpi=450)
+    fig.savefig(output_path / f'{tag}perf_for_top_50_every_epoch.pdf', dpi=450, bbox_inches='tight')
     plt.show()
 
     ## Param-size vs Performance
     fig, ax = generate_scatter_plot(ev_data_df, rs_data_df, 
                                     x_col='params', x_label='Number of Parameters (in Millions)', 
-                                    perf_col=perf_col)
+                                    perf_col=metric.col_name)
     # plt.tight_layout()
-    plt.savefig(output_path / f'{tag}perf_vs_params_for_highperf_cand.png', dpi=450)
+    plt.savefig(output_path / f'{tag}perf_vs_params_for_highperf_cand.pdf', dpi=450, bbox_inches='tight')
     plt.show()
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--input-folder',
+                        type=str, nargs='?',
+                        help='path of folder with evolution and random search results')
     parser.add_argument('--input-path',
                         type=str, nargs='?',
-                        help='path of file with evolution search results')
+                        help='path of file with random search results')
     parser.add_argument('--input-path-random',
                         type=str, nargs='?',
                         help='path of file with random search results')
     parser.add_argument('--result_dir', type=str, nargs='?',
                         help='name of folder where plot files will be dumped')
-    parser.add_argument('--tag', type=str, nargs='?',
-                        help='Tag to prepend to output filename')
+    parser.add_argument('--task', type=str, nargs='?',
+                        help='The identifier for the task. One of (cifar100, roberta, swinir)')
     args = parser.parse_args()
 
-    plot_all(args.input_path, args.input_path_random, args.result_dir)
+    task = args.task
+    input_path_evolution = args.input_path
+    if input_path_evolution is None:
+        input_path_evolution = pathlib.Path(args.input_folder) / f'{task}_search.json'
+
+    input_path_random = args.input_path
+    if input_path_random is None:
+        input_path_random = pathlib.Path(args.input_folder) / f'{task}_search_random.json'
+
+    result_dir = args.result_dir
+    if result_dir is None:
+        result_dir = args.input_folder
+
+    metric = TASK_TO_METRIC[task]
+
+    plot_all(
+        input_path_evolution, 
+        input_path_random, 
+        result_dir,
+        metric,
+        tag=task
+    )
 
 
 if __name__ == '__main__':
