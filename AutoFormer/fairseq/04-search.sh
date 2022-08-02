3447b73b025afa2bb4b57a5415041db002c64842
Ajesh <kmajeshkrishnan@gmail.com>
2022-08-02 16:43:02 +0200

    Scripts are created

--- AutoFormer/fairseq/04-search.sh
+++ AutoFormer/fairseq/04-search.sh
@@ -1,4 +1,4 @@
-fairseq-search-validate common_eval.path='/work/dlclarge1/sharmaa-dltrans/AutoFormer/AutoFormer/fairseq/multirun/2022-07-25/13-22-16/0/checkpoints/checkpoint_best.pt' --task 'masked_lm' --batch-size 1 
+fairseq-search-validate common_eval.path='$(pwd)/multirun/2022-07-25/13-22-16/0/checkpoints/checkpoint_best.pt' --task 'masked_lm' --batch-size 1 
 #--config-dir examples/roberta/config/pretraining \
 #      --config-name small
 #data '' #common_eval.path='/work/dlclarge1/sharmaa-dltrans/AutoFormer/AutoFormer/fairseq/multirun/2022-07-25/13-22-16/0/checkpoints/checkpoint_best.pt' # "$(pwd)/data-bin/wikitext-103"
