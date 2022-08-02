a8cf5c0aa73658295ea457895f1ac043dafe27eb
ayushi-3536 <ayushi-3536@github.com>
2022-08-02 16:02:24 +0200

    exp setup

--- AutoFormer/fairseq/03-eval-test.sh
+++ AutoFormer/fairseq/03-eval-test.sh
@@ -1,4 +1,4 @@
 # Change the model path as needed
 fairseq-validate "$(pwd)/data-bin/wikitext-103" --task "masked_lm" \
---path "$(pwd)/multirun/2022-07-25/00-33-55/0/checkpoints/checkpoint_last.pt" \
+--path "$(pwd)/multirun/2022-08-01/00-21-42/0/retrain/checkpoint_best.pt" \
 --valid-subset test --batch-size 32 --skip-invalid-size-inputs-valid-test
